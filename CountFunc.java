d51bd73f5f7ed612efb6cb9eea9bc1daf5035c9a
jose1 <jfernandezschez@gmail.com>
2022-05-23 17:36:22 +0200

    Commit Count Func

--- CountFunc.java
+++ CountFunc.java
@@ -3,8 +3,8 @@ public class CountFunc {
     public static void printCount() {
 
         System.out.println("Contador");
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(" "+i);
+        for (int i = 1; i <= 1000; i++) {
+            System.out.print(" "+i);
         }
     }
 }
